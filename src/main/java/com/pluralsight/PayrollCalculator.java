651a894dd1a2bb7fe546d8039c2c3caf64c3ba9a
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-08 11:25:01 -0400

    add file writing functionality to createEmployees() to store data in .csv file

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -5,23 +5,7 @@ import java.io.*;
 public class PayrollCalculator {
     public static void main(String[] args) {
 
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader("employees.csv"));
-            String input = bufferedReader.readLine();
-
-            while ((input = bufferedReader.readLine()) != null) {
-                // do stuff
-                System.out.println("New employee: ");
-                String[] employeeData = input.split("\\|");
-                Employee employee = new Employee(Integer.parseInt(employeeData[0]), employeeData[1], Double.parseDouble(employeeData[2]), Double.parseDouble(employeeData[3]));
-                displayEmployee(employee);
-
-            }
-
-        } catch (IOException e) {
-            System.out.println("IO Error?");
-            e.printStackTrace();
-        }
+        createEmployees();
 
     }
     public static void displayEmployee(Employee employee) {
@@ -31,5 +15,33 @@ public class PayrollCalculator {
                 Gross pay: $%.2f
                 
                 """, employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
+    }
+
+    public static void createEmployees() {
+
+        try (
+                BufferedReader bufferedReader = new BufferedReader(new FileReader("employees.csv"));
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("payroll.csv"))
+        ) {
+
+            String input = bufferedReader.readLine();
+            bufferedWriter.write("id|name|gross pay");
+
+            while ((input = bufferedReader.readLine()) != null) {
+                // do stuff
+                System.out.println("New employee: ");
+                String[] employeeData = input.split("\\|");
+                Employee employee = new Employee(Integer.parseInt(employeeData[0]), employeeData[1], Double.parseDouble(employeeData[2]), Double.parseDouble(employeeData[3]));
+                displayEmployee(employee);
+
+                System.out.println("Saving to payroll.csv");
+;               bufferedWriter.write("\n" + employee.getEmployeeId() + "|" + employee.getName() + "|" + employee.getGrossPay());
+
+            }
+
+        } catch (IOException e) {
+            System.out.println("IO Error?");
+            e.printStackTrace();
+        }
     }
 }
