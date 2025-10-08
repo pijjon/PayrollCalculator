5eb052d7b08ae9a9d14c9deb991a2fafdfa92eb8
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-08 12:11:43 -0400

    add comments to PayrollCalculator for better documentation

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -5,15 +5,53 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PayrollCalculator {
+    // declare static scanner so static methods can access it
     public static Scanner myScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+        // prompt user for file names
         String inputFile = askUserStr("Enter the name of the employee file to process: ");
         String outputFile = askUserStr("Enter the name of payroll file to create: ");
 
+        // invoke createEmployees to begin read/write
         createEmployees(inputFile, outputFile);
 
     }
+
+    public static void createEmployees(String inputFile, String outputFile) {
+
+        try (
+                // resources initialized in try with resources will be close automatically
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile))
+        ) {
+
+            String input = bufferedReader.readLine(); // kill the first line header in the input file
+            bufferedWriter.write("id|name|gross pay"); // include a header for the output file
+
+            // assign input to the nextLine of the buffer, if it is not empty then run code
+            while ((input = bufferedReader.readLine()) != null) {
+                // parse through input file data strings
+                System.out.println("New employee: ");
+                String[] employeeData = input.split("\\|");
+
+                // instantiate new Employee objects using the data from the input file we parsed
+                Employee employee = new Employee(Integer.parseInt(employeeData[0]), employeeData[1], Double.parseDouble(employeeData[2]), Double.parseDouble(employeeData[3]));
+                displayEmployee(employee); // display the employee data
+
+                // write data to new output file
+                System.out.println("Saving to " + outputFile);
+;               bufferedWriter.write("\n" + employee.getEmployeeId() + "|" + employee.getName() + "|" + employee.getGrossPay());
+            }
+
+        } catch (IOException e) {
+            System.out.println("IO Error?");
+            e.printStackTrace();
+        }
+    }
+
+    // static method for displaying employee data to console
     public static void displayEmployee(Employee employee) {
         System.out.printf("""
                 Employee ID: %d
@@ -23,33 +61,7 @@ public class PayrollCalculator {
                 """, employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
     }
 
-    public static void createEmployees(String inputFile, String outputFile) {
-
-        try (
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile))
-        ) {
-
-            String input = bufferedReader.readLine();
-            bufferedWriter.write("id|name|gross pay");
-
-            while ((input = bufferedReader.readLine()) != null) {
-                // do stuff
-                System.out.println("New employee: ");
-                String[] employeeData = input.split("\\|");
-                Employee employee = new Employee(Integer.parseInt(employeeData[0]), employeeData[1], Double.parseDouble(employeeData[2]), Double.parseDouble(employeeData[3]));
-                displayEmployee(employee);
-
-                System.out.println("Saving to payroll.csv");
-;               bufferedWriter.write("\n" + employee.getEmployeeId() + "|" + employee.getName() + "|" + employee.getGrossPay());
-
-            }
-
-        } catch (IOException e) {
-            System.out.println("IO Error?");
-            e.printStackTrace();
-        }
-    }
+    // static method for prompting user for input, just pass in the question you want to ask
     public static String askUserStr(String question) {
         String output = null;
         try {
